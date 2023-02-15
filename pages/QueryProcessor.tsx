9bdc4ab875ce4c108d0bdd857a87c1da393f75a0
Anuda Weerasinghe <anuda@live.com>
2023-02-15 13:11:56 -0500

    implements basic QueryProcessor logic

--- pages/QueryProcessor.tsx
+++ pages/QueryProcessor.tsx
@@ -1,3 +1,11 @@
 export default function QueryProcessor(query: string): string {
-  return query;
+  if (query.toLowerCase().includes("shakespeare")) {
+    return (
+      "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+      "English poet, playwright, and actor, widely regarded as the greatest " +
+      "writer in the English language and the world's pre-eminent dramatist."
+    );
+  }
+
+  return "";
 }
