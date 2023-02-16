5d191a0592ccd1546a23217935a999c524a8b945
Victor Huang <victorhuangwq@gmail.com>
2023-02-15 19:54:52 -0500

    Adds jest and test case

---
+++ __tests__/utils/QueryProcessor.test.ts
@@ -0,0 +1,20 @@
+import QueryProcessor from "../../utils/QueryProcessor";
+import '@testing-library/jest-dom'
+
+describe("QueryProcessor", () => {
+    test("should return a string", () => {
+        const query = "test";
+        const response = QueryProcessor(query);
+        expect(typeof response).toBe("string");
+    });
+
+    test('should return shakespeare description', () => {
+        const query = "shakespeare";
+        const response = QueryProcessor(query);
+        expect(response).toBe((
+            "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+            "English poet, playwright, and actor, widely regarded as the greatest " +
+            "writer in the English language and the world's pre-eminent dramatist."
+          ));
+    });
+});
---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  moduleNameMapper: {
+    '@/(.*)$': '<rootDir>/src/$1',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+module.exports = createJestConfig(customJestConfig)
