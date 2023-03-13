54fb5b458562148909cd39a21328c8aa2bcead9b
teresa0605 <teresawan2018@gmail.com>
2023-03-13 16:37:27 -0400

    Changed heading to Hello

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
   return (
     <div>
-      <h1>Welcome!!</h1>
+      <h1>Hello!!</h1>
       <p>Please enter your query in the box below:</p>
       <input
         type="text"
