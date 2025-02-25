5655ee3740e5053e8fe3f26299923c6af61fd6c5
TJ Patel <tjpatel@tjs-mbp-2.wifi.local.cmu.edu>
2025-02-24 22:32:35 -0500

    Added query change to include Andrew ID

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -11,5 +11,9 @@ export default function QueryProcessor(query: string): string {
     return "Rohan";
   }
 
+  if (query.toLowerCase().includes("ID")) {
+    return "tpatel"
+  }
+
   return "";
 }
