c010ac80bcca7876b0461e76446210a5b46d1223
Sean Jiang <songyij@andrew.cmu.edu>
2025-10-06 10:31:26 -0400

    task 2 done  + tests

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -2,27 +2,31 @@ import QueryProcessor from "../../utils/QueryProcessor";
 import '@testing-library/jest-dom'
 
 describe("QueryProcessor", () => {
-    test("should return a string", () => {
-        const query = "test";
-        const response: string = QueryProcessor(query);
-        expect(typeof response).toBe("string");
-    });
+  test("should return a string", () => {
+    const query = "test";
+    const response: string = QueryProcessor(query);
+    expect(typeof response).toBe("string");
+  });
 
-    test('should return shakespeare description', () => {
-        const query = "shakespeare";
-        const response: string = QueryProcessor(query);
-        expect(response).toBe((
-            "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-            "English poet, playwright, and actor, widely regarded as the greatest " +
-            "writer in the English language and the world's pre-eminent dramatist."
-          ));
-    });
+  test('should return shakespeare description', () => {
+    const query = "shakespeare";
+    const response: string = QueryProcessor(query);
+    expect(response).toBe((
+      "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+      "English poet, playwright, and actor, widely regarded as the greatest " +
+      "writer in the English language and the world's pre-eminent dramatist."
+    ));
+  });
 
-    test('should return name', () => {
-        const query = "What is your name?";
-        const response: string = QueryProcessor(query);
-        expect(response).toBe((
-            "Rohan"
-          ));
-    })
+  test('should return name', () => {
+    const query = "What is your name?";
+    const response: string = QueryProcessor(query);
+    expect(response).toBe("Rohan");
+  });
+
+  test('should return Andrew ID', () => {
+    const query = "What is your Andrew ID?";
+    const response: string = QueryProcessor(query);
+    expect(response).toBe("songyij");
+  });
 });
--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,5 +1,7 @@
 export default function QueryProcessor(query: string): string {
-  if (query.toLowerCase().includes("shakespeare")) {
+  const q = query.toLowerCase().trim();
+
+  if (q.includes("shakespeare")) {
     return (
       "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
       "English poet, playwright, and actor, widely regarded as the greatest " +
@@ -7,7 +9,11 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
-  if (query.toLowerCase().includes("name")) {
+  if (q.includes("andrew id") || q.includes("andrewid")) {
+    return "songyij";
+  }
+
+  if (q.includes("name")) {
     return "Rohan";
   }
 
